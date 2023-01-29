2d6e0b3ab713b60be9d6d26bdc719ffe43f01e47
WifeCo <142204664+WifeCo@users.noreply.github.com>
2023-01-29 18:52:45 +0000

    added social icons

--- src/GPTFunctions.ts
+++ src/GPTFunctions.ts
@@ -2,4 +2,5 @@ import { Configuration, OpenAIApi } from 'openai'
 import { cleanCode, getContentString, getDefaultCreateOptions } from './utils'
 
 /**
- * Options for getting a result from the 
+ * Options for getting a result from the OpenAI API
+ * @temp
