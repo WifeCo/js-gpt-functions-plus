9b6dfdebf913281b8dee35f067642398e7c5b1f5
WifeCo <142204664+WifeCo@users.noreply.github.com>
2023-05-06 20:05:38 +0000

    Added roadmap

--- src/GPTFunctions.ts
+++ src/GPTFunctions.ts
@@ -72,4 +72,5 @@ export class GPTFunctions {
      *  @returns {Promise<T>} - A promise that resolves to the result of calling the function.
      *  @template T
      */
-    public getResult = async <T = 'string
+    public getResult = async <T = 'string'>({
+        func,
