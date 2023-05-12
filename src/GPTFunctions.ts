a8ff9e27571c3960a601bd2f2a4e2ddcc4119ee8
WifeCo <142204664+WifeCo@users.noreply.github.com>
2023-05-12 11:21:39 +0000

    Traduction

--- src/GPTFunctions.ts
+++ src/GPTFunctions.ts
@@ -79,4 +79,4 @@ export class GPTFunctions {
         model = 'gpt-3.5-turbo',
         postProcess = (res: unknown) => res as T
     }: ResOptions<T>): Promise<T> => {
-      
+        const response
