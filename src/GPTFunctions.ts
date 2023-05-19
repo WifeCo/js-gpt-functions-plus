3dbd40c330bf2c7a40a90d48a631181e829656f3
WifeCo <142204664+WifeCo@users.noreply.github.com>
2023-05-19 15:25:57 +0000

    update readme files

--- src/GPTFunctions.ts
+++ src/GPTFunctions.ts
@@ -97,4 +97,4 @@ export class GPTFunctions {
 
     /**
      *  Generate a function using inly it's description
-     *  @param {CreateFucntionOptions} options - The
+     *  @param {CreateFucntionOptions} options - The options for gener
