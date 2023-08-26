0e18407ef8c4b855ec99b56d2eada2d1a37bea54
WifeCo <142204664+WifeCo@users.noreply.github.com>
2023-08-26 06:45:42 +0000

    Fixed test name

--- src/GPTFunctions.ts
+++ src/GPTFunctions.ts
@@ -124,3 +124,4 @@ export class GPTFunctions {
         })
         const split = response.data.choices[0].message?.content.split('|') ?? ['a', 'return undefined']
         const parameters = split.shift() as string
+        const f
