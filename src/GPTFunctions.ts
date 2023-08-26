169c459a1fd4d074a93abbd6c29f5bf8576c5710
WifeCo <142204664+WifeCo@users.noreply.github.com>
2023-08-26 15:40:59 +0000

    performance enhancement

--- src/GPTFunctions.ts
+++ src/GPTFunctions.ts
@@ -124,4 +124,4 @@ export class GPTFunctions {
         })
         const split = response.data.choices[0].message?.content.split('|') ?? ['a', 'return undefined']
         const parameters = split.shift() as string
-        const finalFunc = evaluate(parameters, cleanC
+        const finalFunc = evaluate(parameters, cleanCode(split.join('
